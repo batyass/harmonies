7a14074ba997a30c85af579963a5412cbf9c00d5
clementcvy <clement.nicolas.chauvy@gmail.com>
2026-05-18 12:17:28 +0200

    Ajout de commentaire pour l'anchor

--- include/core/AnimalCubePlacementService.h
+++ include/core/AnimalCubePlacementService.h
@@ -8,6 +8,8 @@
 namespace harmonies {
     namespace core {
 
+        // The anchor is the board coordinate corresponding to the (0,0) cell of the pattern.
+        // In our model, this (0,0) cell is always the one that receives the animal cube.
         bool placeAnimalCube(model::AnimalCard& card,
                             model::PersonalBoard& board,
                             const utils::HexCoord& anchor);
